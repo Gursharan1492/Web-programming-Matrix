111c734d337821a34b5e6f0e9e01e9ebe301b23b
Gursharanpreet Singh <92213569+Gursharan1492@users.noreply.github.com>
2021-11-07 19:46:58 -0500

    Assignment 9

---
+++ Gursharanpreet_Singh_5369473.php
@@ -0,0 +1,28 @@
+<?php
+  $p=$_POST['product'];
+  $q=$_POST['quantity'];
+  $d=$_POST['date'];
+  $sa1=$_POST['saddress1'];
+  $sa2=$_POST['saddress2'];
+  $c=$_POST['city'];
+  $reg=$_POST['rigion'];
+  $pin=$_POST['pin'];
+  $con=$_POST['country'];
+  $fn=$_POST['fname'];
+  $ln=$_POST['lname'];
+  $mob=$_POST['mobile'];
+  $email=$_POST['email'];
+  
+  echo $p."<br>";
+  echo $q."<br>";
+  echo $d."<br>";
+  echo $sa1."<br>";
+  echo $sa2."<br>";
+  echo $c."<br>";
+  echo $reg."<br>";
+  echo $pin."<br>";
+  echo $fn."<br>";
+  echo $ln."<br>";
+  echo $mob."<br>";
+  echo $email."<br>";
+?>
